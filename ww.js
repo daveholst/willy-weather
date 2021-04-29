6ebf3013a27f0c9892355abcf6d01873ce9e90c4
Dave Holst <daveholst@gmail.com>
2021-04-29 09:52:50 +0800

    basic module exporting

--- ww.js
+++ ww.js
@@ -1,1 +1,14 @@
 // test function
+
+// const wwObject = {
+//   weatherToday(postcode) {
+//     console.log(`INSIDE OBJ: weather today @ ${postcode} is: FINE`);
+//   },
+// };
+const weatherToday = (postcode, wwAPI) => {
+  console.log(`OUTSIDE OBJ: weather today @ ${postcode} is: FINE`);
+  console.log(wwAPI);
+};
+
+// module.exports.wwObject = wwObject;
+module.exports.weatherToday = weatherToday;
